5bae5de0c165d3828e22b60f48a8406f6a706f9c
RyanSwitzercock <hyperluminous23@gmail.com>
2024-07-10 01:52:58 -0400

    Update downloads.sh

--- downloads.sh
+++ downloads.sh
@@ -1,5 +1,1 @@
-sudo pacman -Sy picom python-pywal feh discord fastfetch steam brave-bin base-devel git vim xorg-server xorg-xinit libx11 libxinerama libxft webkit2gtk jre8-openjdk jre21-openjdk jre17-openjdk lxappearance ttf-roboto &
-
-git clone https://aur.archlinux.org/yay.git &
-
-echo "######## Do \"makepkg -si \" in the yay directory to finish installing yay. ########"
+sudo pacman -Sy picom python-pywal feh discord fastfetch steam base-devel git vim xorg-server xorg-xinit libx11 libxinerama libxft webkit2gtk jre8-openjdk jre21-openjdk jre17-openjdk lxappearance ttf-roboto 
