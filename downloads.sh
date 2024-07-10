20d10f23955ff66c0f74c448294c957325bb8c3f
RyanSwitzercock <hyperluminous23@gmail.com>
2024-07-10 01:12:15 -0400

    Update downloads.sh

--- downloads.sh
+++ downloads.sh
@@ -1,4 +1,4 @@
-sudo pacman -Sy picom python-pywal feh discord fastfetch steam brave-bin base-devel git vim xorg-server xorg-xinit libx11 libxinerama libxft webkit2gtk jre8-openjdk jre21-openjdk jre17-openjdk lxappearance &
+sudo pacman -Sy picom python-pywal feh discord fastfetch steam brave-bin base-devel git vim xorg-server xorg-xinit libx11 libxinerama libxft webkit2gtk jre8-openjdk jre21-openjdk jre17-openjdk lxappearance ttf-roboto &
 
 git clone https://aur.archlinux.org/yay.git &
 
