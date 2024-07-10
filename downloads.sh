ccf8c5773ed40e9aec2898265a32719c924c215e
RyanSwitzercock <hyperluminous23@gmail.com>
2024-07-10 00:56:28 -0400

    Add files via upload

---
+++ downloads.sh
@@ -0,0 +1,5 @@
+sudo pacman -Sy picom python-pywal feh discord fastfetch steam brave-bin base-devel git vim xorg-server xorg-xinit libx11 libxinerama libxft webkit2gtk jre8-openjdk jre21-openjdk jre17-openjdk &
+
+git clone https://aur.archlinux.org/yay.git &
+
+echo "######## Do \"makepkg -si \" in the yay directory to finish installing yay. ########"
