7e47333712d0d9d3326e61d2397846767b1c3fce
RyanSwitzercock <hyperluminous23@gmail.com>
2024-07-27 19:08:04 -0400

    Add files via upload

--- downloads.sh
+++ downloads.sh
@@ -1,4 +1,4 @@
-sudo pacman -Sy picom python-pywal feh discord fastfetch rofi steam base-devel git vim xorg-server xorg-xinit libx11 libxinerama libxft webkit2gtk jre8-openjdk jre21-openjdk jre17-openjdk lxappearance nemo flameshot libreoffice-fresh ranger obsidian spotify-launcher alacritty obs-studio gparted htop yay &
+sudo pacman -Sy picom python-pywal feh discord fastfetch rofi steam base-devel vim xorg-server xorg-xinit libx11 libxinerama libxft webkit2gtk jre8-openjdk jre21-openjdk jre17-openjdk lxappearance nemo flameshot libreoffice-fresh ranger obsidian spotify-launcher alacritty obs-studio gparted htop yay &
 
 
 echo "######## Do \"makepkg -si \" in the yay directory to finish installing yay. ########"
---
+++ git-downloads.sh
@@ -0,0 +1,2 @@
+git clone https://github.com/RyanSwitzercock/rswk_mydwm.git &
+git clone https://github.com/Morganamilo/paru.git
