a5659422ba26d8db0a5af40894f3f3c2c52b3816
RyanSwitzercock <hyperluminous23@gmail.com>
2024-08-03 01:28:34 -0400

    Add files via upload

---
+++ dmenu/colors-wal-dmenu.h
@@ -0,0 +1,6 @@
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#efd2da", "#2b2545" },
+	[SchemeSel] = { "#efd2da", "#475292" },
+	[SchemeOut] = { "#efd2da", "#EE9CB0" },
+};
